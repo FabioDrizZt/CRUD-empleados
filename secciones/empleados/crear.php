50b1d8d5064207973abe18b8c74595cc859e6dbf
Fabi0x <Fabi0x@argentina programa.com>
2023-08-02 23:08:00 -0300

    Agregado el alta de empleados

--- secciones/empleados/crear.php
+++ secciones/empleados/crear.php
@@ -1,3 +1,63 @@
+<?php
+require_once '../../bd.php';
+
+if ($_POST) {
+/*   echo "<pre>";
+  print_r($_POST);
+  print_r($_FILES); */
+
+  // Lo sacamos de $_POST
+  $primernombre = $_POST["primernombre"];
+  $segundonombre = $_POST["segundonombre"];
+  $primerapellido = $_POST["primerapellido"];
+  $segundoapellido = $_POST["segundoapellido"];
+  $idpuesto = $_POST["idpuesto"];
+  // Lo sacamos de $_FILES
+  $foto = isset($_FILES["foto"]["name"]) ? $_FILES["foto"]["name"] : "";
+  $cv = $_FILES["cv"]["name"];
+
+  $sentencia = $conexion->prepare("INSERT INTO `tbl_empleados`
+         (`primernombre`, `segundonombre`, `primerapellido`, `segundoapellido`, `foto`, `cv`, `idpuesto`) 
+  VALUES (:primernombre, :segundonombre, :primerapellido, :segundoapellido, :foto, :cv, :idpuesto)");
+
+  $sentencia->bindParam(':primernombre', $primernombre);
+  $sentencia->bindParam(':segundonombre', $segundonombre);
+  $sentencia->bindParam(':primerapellido', $primerapellido);
+  $sentencia->bindParam(':segundoapellido', $segundoapellido);
+  $sentencia->bindParam(':idpuesto', $idpuesto);
+
+  $nombreArchivo_foto = "";
+  $fecha = new DateTime();
+  if ($foto != '') {
+    //le creo un nombre unico a la foto adjuntandole la fecha y hora actual
+    $nombreArchivo_foto = $fecha->getTimestamp() . "_" . $_FILES["foto"]["name"];
+    // busco la ruta en la que se aloja temporalmente la foto
+    $tmp_foto = $_FILES["foto"]["tmp_name"];
+    // muevo la foto desde la ruta temporal hasta mi carpeta de imagenes
+    move_uploaded_file($tmp_foto, "./assets/img/" . $nombreArchivo_foto);
+  }
+  $nombreArchivo_cv = "";
+  if ($cv != '') {
+    //le creo un nombre unico al cv adjuntandole la fecha y hora actual
+    $nombreArchivo_cv = $fecha->getTimestamp() . "_" . $_FILES["cv"]["name"];
+    // busco la ruta en la que se aloja temporalmente el cv
+    $tmp_cv = $_FILES["cv"]["tmp_name"];
+    // muevo la cv desde la ruta temporal hasta mi carpeta de pdfs
+    move_uploaded_file($tmp_cv, "./assets/pdf/" . $nombreArchivo_cv);
+  }
+  $sentencia->bindParam(':foto', $nombreArchivo_foto);
+  $sentencia->bindParam(':cv', $nombreArchivo_cv);
+
+  $sentencia->execute();
+
+  header("Location:index.php");
+}
+
+$sentencia = $conexion->prepare("SELECT * FROM `tbl_puestos`");
+$sentencia->execute();
+$lista_tbl_puestos = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+
+?>
 <?php require_once '../../templates/header.php' ?>
 
 <div class="container">
@@ -6,7 +66,7 @@
       <h3>Creación de empleado</h3>
     </div>
     <div class="card-body">
-      <form>
+      <form method="POST" enctype="multipart/form-data">
         <div class="mb-3">
           <label for="primernombre" class="form-label">Primer Nombre</label>
           <input type="text" class="form-control" name="primernombre" id="primernombre" aria-describedby="helpId" placeholder="">
@@ -32,11 +92,12 @@
           <input type="file" class="form-control" name="cv" id="cv" placeholder="" aria-describedby="fileHelpId">
         </div>
         <label for="puesto" class="form-label">Puesto:</label>
-        <select class="form-select" aria-label="Default select example">
+        <select name="idpuesto" class="form-select" aria-label="Default select example">
           <option disabled selected>Seleccione su puesto</option>
-          <option value="1">Uno</option>
-          <option value="2">Dos</option>
-          <option value="3">Tres</option>
+          <?php foreach ($lista_tbl_puestos as $registro) { ?>
+            <option value=<?= $registro['id'] ?>><?= $registro['nombredelpuesto'] ?></option>
+          <?php } ?>
+
         </select>
         <br>
         <a class="btn btn-danger" href="./index.php" role="button">Cancelar</a>
--- secciones/empleados/index.php
+++ secciones/empleados/index.php
@@ -1,3 +1,13 @@
+<?php require_once '../../bd.php';
+
+$sentencia = $conexion->prepare("SELECT *, (
+  SELECT nombredelpuesto
+  FROM tbl_puestos
+  WHERE tbl_puestos.id=tbl_empleados.idpuesto
+  ) as puesto FROM `tbl_empleados`");
+$sentencia->execute();
+$lista_tbl_empleados = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+?>
 <?php require_once '../../templates/header.php' ?>
 
 <div class="container">
@@ -13,6 +23,7 @@
         <table aria-label="tabla de empleados" class="table ">
           <thead>
             <tr>
+              <th scope="col">#</th>
               <th scope="col">Nombre</th>
               <th scope="col">Foto</th>
               <th scope="col">CV</th>
@@ -22,18 +33,31 @@
             </tr>
           </thead>
           <tbody>
-            <tr class="">
-              <td>Fabio D. Argañaraz</td>
-              <td>imagen.jpg</td>
-              <td>CV.pdf</td>
-              <td>Profesor</td>
-              <td>12/12/2019</td>
-              <td>
-                <a class="btn btn-success" href="#" role="button">Carta</a>
-                <a class="btn btn-info" href="./editar.php" role="button">Editar</a>
-                <a class="btn btn-danger" href="#" role="button">Eliminar</a>
-              </td>
-            </tr>            
+            <?php foreach ($lista_tbl_empleados as $registro) { ?>
+              <tr class="">
+                <td><?= $registro['id'] ?></td>
+                <td>
+                  <?= $registro['primernombre'] ?>
+                  <?= $registro['segundonombre'] ?>
+                  <?= $registro['primerapellido'] ?>
+                  <?= $registro['segundoapellido'] ?>
+                </td>
+                <td>
+                  <img class="img-fluid rounded-top" src="<?= "./assets/img/" .  $registro['foto'] ?>" alt="foto-perfil" width=100 />
+                </td>
+                <td>
+                  <a href="<?= "./assets/pdf/" .  $registro['cv'] ?>" download="<?= "CV-" . $registro['primernombre'] . $registro['primerapellido'] ?>">CV</a>
+                </td>
+                <td><?= $registro['puesto'] ?></td>
+                <td><?= $registro['fechadeingreso'] ?></td>
+                <td>
+                  <a class="btn btn-success" href="#" role="button">Carta</a>
+                  <a class="btn btn-info" href="./editar.php" role="button">Editar</a>
+                  <a class="btn btn-danger" href="#" role="button">Borrar</a>
+                </td>
+              </tr>
+            <?php } ?>
+
           </tbody>
         </table>
       </div>
